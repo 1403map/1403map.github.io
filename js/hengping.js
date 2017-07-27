6baf81fd297747baab5e4836c4f3425ffe664329
1403map <geekdark20@yahoo.com>
2017-07-27 14:24:21 +0800

    一起上大学

--- js/hengping.js
+++ js/hengping.js
@@ -1,5 +1,1 @@
-window.onload = function(){
-  window.event? window.event.cancelBubble = true : e.stopPropagation();
-  // document.documentElement.requestFullScreen();
-  screen.orientation.lock("portrait-primary");
-}
+
