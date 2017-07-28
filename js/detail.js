19f5166c2cb27cdc0ca14e5656792b81e5e48231
1403map <geekdark20@yahoo.com>
2017-07-28 10:39:53 +0800

    一起上大学

--- js/detail.js
+++ js/detail.js
@@ -1,8 +1,70 @@
-var student=[
+var student=[{},{xingming:'曹春丽',daxue:'广西财经学院',zhuanye:'',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'陈春莲',daxue:'广西科技大学',zhuanye:'工业设计',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'陈佳锐',daxue:'广西电力职业技术学院',zhuanye:'',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'陈家诚',daxue:'广西大学',zhuanye:'动物科学',xiaohui:'xiaohui/西大.jpg'},
+  {xingming:'陈景德',daxue:'玉林师范学院',zhuanye:'',xiaohui:'xiaohui/武科大.jpg'},
   {},
-  {},
-  {xingming:'冯光雁',daxue:'武汉科技大学',zhuanye:'安全工程'},
-  {xingming:'邓子秋',daxue:'广西大学',zhuanye:'农学'}
+  {xingming:'陈维嘉',daxue:'',zhuanye:'',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'陈信峄',daxue:'南宁职业学院',zhuanye:'',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'陈勇志',daxue:'柳铁',zhuanye:'',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'陈宇翔',daxue:'桂林电子科技大学',zhuanye:'交通工程',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'陈宗良',daxue:'山西财经大学',zhuanye:'',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'邓凯文',daxue:'',zhuanye:'',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'邓子秋',daxue:'广西大学',zhuanye:'农学',xiaohui:'xiaohui/西大.jpg'},
+  {xingming:'杜海泉',daxue:'',zhuanye:'',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'范威虎',daxue:'哈尔滨师范大学',zhuanye:'化学',xiaohui:'xiaohui/哈师大.jpg'},
+  {xingming:'冯光雁',daxue:'武汉科技大学',zhuanye:'安全工程',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'何健蔚',daxue:'广西大学行健文理学院',zhuanye:'土木工程',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'黄浩然',daxue:'大连大学',zhuanye:'软件工程',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'黄启航',daxue:'上海工程技术大学',zhuanye:'轨道工程',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'蒋宗沛',daxue:'福建农林大学',zhuanye:'',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'黎其',daxue:'',zhuanye:'',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'李楚文',daxue:'广西大学行健文理学院',zhuanye:'',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'李佳芹',daxue:'广西师范大学',zhuanye:'英语',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'李诗莹',daxue:'',zhuanye:'',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'李显安',daxue:'桂林航天工业学院',zhuanye:'',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'李玉君',daxue:'泉州师范学院',zhuanye:'化工与制药类',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'李重豪',daxue:'玉林师范学院',zhuanye:'',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'梁东',daxue:'',zhuanye:'',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'梁崑伟',daxue:'',zhuanye:'',xiaohui:''},
+  {xingming:'梁璐璐',daxue:'',zhuanye:'',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'林传洲',daxue:'',zhuanye:'',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'林帆',daxue:'广西名族大学',zhuanye:'体育学类',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'罗伟豪',daxue:'福建省龙泉学院',zhuanye:'',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'吕伊羚',daxue:'广西科技大学',zhuanye:'化学工程与工艺',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'莫振淼',daxue:'',zhuanye:'',xiaohui:''},
+  {xingming:'牟壮志',daxue:'',zhuanye:'',xiaohui:''},
+  {xingming:'庞栋才',daxue:'广西财经学院',zhuanye:'',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'庞兆钦',daxue:'南宁职业学院',zhuanye:'',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'唐伯伦',daxue:'',zhuanye:'',xiaohui:''},
+  {xingming:'唐依莹',daxue:'广西财经学院',zhuanye:'投资学',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'唐雨菲',daxue:'柳州铁路职业技术学院',zhuanye:'铁路工程',xiaohui:'xiaohui/柳铁.jpg'},
+  {xingming:'田展翔',daxue:'东北农业大学',zhuanye:'土木工程',xiaohui:'xiaohui/东农.jpg'},
+  {xingming:'王荃生',daxue:'',zhuanye:'',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'王维维',daxue:'沈阳药科大学',zhuanye:'制药工程',xiaohui:'xiaohui/沈药.jpg'},
+  {xingming:'魏宁',daxue:'',zhuanye:'',xiaohui:''},
+  {xingming:'温靖铭',daxue:'南宁学院',zhuanye:'电气工程',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'吴佳俊',daxue:'梧州学院',zhuanye:'',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'谢远筹',daxue:'',zhuanye:'',xiaohui:''},
+  {xingming:'晏志芳',daxue:'广西科技大学',zhuanye:'工程管理',xiaohui:'xiaohui/广西科技大.jpg'},
+  {xingming:'杨湘琼',daxue:'北京农学院',zhuanye:'生物技术',xiaohui:'xiaohui/北京农学院.jpg'},
+  {xingming:'杨晓夏',daxue:'广西财经学院',zhuanye:'',xiaohui:'xiaohui/广西财经.jpg'},
+  {xingming:'杨新',daxue:'广西师范学院',zhuanye:'',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'杨业群',daxue:'',zhuanye:'',xiaohui:''},
+  {xingming:'姚俊云',daxue:'安徽师范大学',zhuanye:'',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'余可威',daxue:'福建师范大学',zhuanye:'',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'袁子凯',daxue:'广西财经学院',zhuanye:'经济统计',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'曾德键',daxue:'',zhuanye:'',xiaohui:''},
+  {xingming:'张钧怡',daxue:'',zhuanye:'',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'张凯奇',daxue:'',zhuanye:'',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'张宇红',daxue:'广西财经学院',zhuanye:'',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'张智',daxue:'',zhuanye:'',xiaohui:''},
+  {xingming:'钟承峻',daxue:'',zhuanye:'',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'钟巧秋',daxue:'桂林航天工业',zhuanye:'电子信息工程',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'周传勇',daxue:'南宁职业学院',zhuanye:'',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'周恒宇',daxue:'广西名族大学相思湖学院',zhuanye:'',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'周培文',daxue:'岭南师范学院',zhuanye:'化学',xiaohui:'xiaohui/武科大.jpg'},
+  {xingming:'周杨',daxue:'广西财经学院',zhuanye:'工商管理',xiaohui:'xiaohui/武科大.jpg'}
 ];
 
 function daxue(xuehao) {
@@ -12,11 +74,9 @@ function daxue(xuehao) {
   $(".xingming_1").append(student[xuehao].xingming);
   $(".xuexiao_1").append(student[xuehao].daxue);
   $(".zhuanye_1").append(student[xuehao].zhuanye);
+  $("#xiaohui").attr("src",student[xuehao].xiaohui);
 }
 
 // function yichu() {
 //
 // }
-function yichu() {
-  alert("你离开了");
-}
