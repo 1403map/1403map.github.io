21a738e320e34b3a4b65e09f4b9728185263d6fa
1403map <geekdark20@yahoo.com>
2017-07-28 15:21:48 +0800

    一起上大学

--- js/autoplay.js
+++ js/autoplay.js
@@ -1,11 +1,13 @@
-async function test() {
-  await sleep(1000);
-}
+// async function test() {
+//   await sleep(1000);
+// }
+//
+// function sleep(ms) {
+//   return new Promise(resolve => setTimeout(resolve, ms));
+// }
+// test();
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-test();
+
 // test();
 // async function test() {
 //   for(var i=1;i<10;i++){
